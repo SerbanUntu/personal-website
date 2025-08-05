e75bf81a9668a3f23e97f60fc83ccce19b438287
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2025-08-05 14:10:49 +0300

    feat: write the home, 404, and project pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 import favicons from 'astro-favicons'
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss(), favicons()],
+  vite: {
+      plugins: [tailwindcss(), favicons()],
 	},
+
+  integrations: [icon()],
 })
